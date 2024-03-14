081ab5acda48e03511d3db01984873795fdb8e96
NMCBU1 <86202623+NMCBU1@users.noreply.github.com>
2024-03-14 15:12:48 -0500

    Update QueryProcessor.tsx

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -7,15 +7,16 @@ export default function QueryProcessor(query: string): string {
     );
   }
   if (query.toLowerCase().includes("what is your name")) {
-    return (
-      "Nicolas M"
-    );
+    return "Nicolas M";
   }
 
   if (query.toLowerCase().includes("which of the following numbers is the largest")) {
-    const numbers = query.match(/\d+/g).map(Number);
-    const largestNumber = Math.max(...numbers);
-    return (`${largestNumber}`);
+    const matches = query.match(/\d+/g);
+    if (matches) {
+      const numbers = matches.map(Number);
+      const largestNumber = Math.max(...numbers);
+      return `${largestNumber}`;
+    }
   }
   return "";
 }

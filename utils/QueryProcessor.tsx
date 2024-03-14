41955729eb0b8389e6a4cb9967e36bb53b4769af
NMCBU1 <86202623+NMCBU1@users.noreply.github.com>
2024-03-14 15:16:44 -0500

    Update QueryProcessor.tsx

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -27,6 +27,15 @@ export default function QueryProcessor(query: string): string {
       return `${product}`;
     }
   }
+
+  if (query.toLowerCase().includes("plus")) {
+    const matches = query.match(/\d+/g);
+    if (matches) {
+      const numbers = matches.map(Number);
+      const product = numbers.reduce((acc, number) => acc + number, 0); // Start the accumulator at 1 for multiplication
+      return `${product}`;
+    }
+  }
   
   return "";
 }
